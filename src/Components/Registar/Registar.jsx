63d84d95d9be44ea3d3ae085c950c7ee20067b16
sifat26 <sifatict26@gmail.com>
2024-04-06 00:48:12 +0600

    Login section added

--- src/Components/Registar/Registar.jsx
+++ src/Components/Registar/Registar.jsx
@@ -1,20 +1,31 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebaseConfig";
 import { useState } from "react";
+import { GoEye } from "react-icons/go";
+import { GoEyeClosed } from "react-icons/go";
 
 const Registar = () => {
     const [registerError,setRegisterError]=useState('')
     const [success,setSuccess] = useState('')
+    const [showPassword,setShowPassword]=useState(false);
     const handleRegister=e=>{
         e.preventDefault();
         const email=e.target.email.value;
         const password=e.target.password.value;
-        console.log(email,password);
+        const accept=e.target.terms.checked;
+        console.log(email,password,accept);
         setRegisterError('')
         setSuccess('')
         if(password.length<6){
             setRegisterError('Password must be at least 6 characters');
             return;
+        }
+        else if(!/[A-Z]/.test(password)){
+            setRegisterError('Password must contain at least one uppercase letter');
+            return;
+        }
+        else if(!accept){
+           setRegisterError('Please Accept terms and Condition'); 
         }
         createUserWithEmailAndPassword(auth,email,password)
         .then(result=>{
@@ -30,21 +41,33 @@ const Registar = () => {
 
     }
     return (
-        <div className="border text-center">
-            <div className="max-auto md:w-full">
+        <div className=" ">
+            <div className="mx-auto md:w-1/2">
             <h2 className="text-3xl">Please Register</h2>
             <form onSubmit={handleRegister}>
                 <input 
                 required
-                className="mb-4 w-3/4 py-2 px-4" type="email" name="email" id="" placeholder="Your Email"/>
+                className="mb-4 w-full py-2 px-4" type="email" name="email" id="" placeholder="Your Email"/>
                 <br />
-                <input 
+                <div className="relative">
+                    <input 
                 required
-                className="mb-4 w-3/4 py-2 px-4"type="password" name="password" id="" placeholder="Your Password"/>
+                className=" w-full py-2 px-4  "
+                type={showPassword?"text":"password" }
+                name="password" id="" placeholder="Your Password"/>
+                <span className=" absolute top-3 right-2" onClick={()=>{
+                    setShowPassword(!showPassword);
+                } }>{showPassword?
+                    <GoEye />:<GoEyeClosed />}</span>
+                </div>
+                
+                <br />
+                <input type="checkbox" name="terms" id="terms" />
+                <label htmlFor="terms"> Accept Our <a href=""> Terms and Condition</a></label>
                 <br />
                 <input 
                 
-                className="mb-8 w-3/4 btn btn-secondary "
+                className="mb-8 w-full btn btn-secondary "
                 type="submit" value="Register" />
             </form>
             {
