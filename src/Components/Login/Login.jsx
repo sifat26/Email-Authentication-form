5af6ed74f0a1f3f68531a4b0d101ba960cfda104
sifat26 <sifatict26@gmail.com>
2024-04-06 01:11:00 +0600

    Log in form update

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,18 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebaseConfig";
+
 const Login = () => {
+    const handleLogin=(e)=>{
+        e.preventDefault();
+        const email=e.target.email.value;
+        const password=e.target.password.value;
+        signInWithEmailAndPassword(auth,email,password)
+        .then(result=>{
+            console.log(result.user);
+        })
+        .catch(error=>console.error(error))
+        
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -7,18 +21,21 @@ const Login = () => {
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+      <form onSubmit={handleLogin} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" 
+          name="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" 
+          name="password"
+          placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
